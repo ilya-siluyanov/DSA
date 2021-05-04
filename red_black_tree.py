f7e7447731ba9dec7ff2ad69d707d2c9c1a9037d
ilyasiluyanov <isiluyanov8@gmail.com>
2021-05-04 23:12:43 +0300

    partial insert_fixup

--- red_black_tree.py
+++ red_black_tree.py
@@ -5,38 +5,82 @@ class Color(Enum):
 
 class Node:
   
-  def __init__(self, value = None, color=Color.BLACK, left=None, right=None):
+  def __init__(self, value=None):
     self.value = value
     self.left = None
     self.right = None
     self.parent = None
-    self.color = color
+    self.color = Color.RED
+  
+  def set_right(self, node):
+    self.right = node
+    node.parent = self
+  
+  def set_left(self, node):
+    self.left = node
+    node.parent = self
 
 
 class RedBlackTree:
  
   def __init__(self):
-    self.nullNode = Node()
-    self.root = self.nullNode
+    self.null_node = Node(None)
+    self.null_node.color = Color.BLACK
+    self.root = self.null_node
   
+  def generate_node(self, value) -> Node:
+    new_node = Node(value)
+    new_node.left = self.null_node
+    new_node.right = self.null_node
+    new_node.parent = self.null_node
+
   def insert(self, value):
-    newNode = Node(value, Color.RED)
-    if self.root is self.nullNode:
-      newNode.parent = self.nullNode
-      self.root = newNode
+    new_node = self.generate_node(value)
+    if self.root is self.null_node:
+      self.root = new_node
     else:
       curr = self.root
-      while curr is not None:
-        if curr.value == newNode.value:
+      while curr is not self.null_node:
+        if curr.value == new_node.value:
           return
-        elif curr.value < newNode.value:
+        elif curr.value < new_node.value:
           if curr.right is None:
-            curr.right = newNode
+            curr.set_right(new_node)
           else:
             curr = curr.right
         else:
           if curr.left is None:
-            curr.left = newNode
+            curr.set_left(new_node)
           else:
             curr = curr.left
-    self.insert_fixup(newNode)
+    self.insert_fixup(new_node)
+
+    def insert_fixup(self, new_node):
+      while new_node.parent.color == Color.RED:
+        #case 1: uncle is red too
+        #case 2: uncle is black, new_node is right child
+        #case 3: similar to c.2, but new_node is left child
+        if new_node.parent.parent.left is new_node.parent: #new_node is a left child
+          uncle = new_node.parent.parent.right
+          if uncle.color == Color.RED:  #case 1
+            uncle.color = Color.BLACK
+            new_node.parent.color = Color.BLACK
+            new_node.parent.parent.color = Color.RED
+            new_node = new_node.parent.parent
+          else:
+            if new_node.parent.right is new_node:  #case 2
+              new_node = new_node.parent
+              self.left_rotation(new_node)
+            new_node.color = Color.BLACK
+            new_node.parent = Color.RED
+            new_node = new_node.parent
+            self.right_rotation(new_node)
+        else: #new_node is a right child
+          pass
+    def left_rotation(self, node):
+      pass
+    def right_rotation(self, node):
+      pass
+
+
+        
